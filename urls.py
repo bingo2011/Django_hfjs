66f77eb201efd5b5e64a658d79917c9743ab6621
bingo2011 <bin.go2011@foxmail.com>
2013-02-11 14:42:51 +0800

    donuts

--- urls.py
+++ urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('',
-    url(r'^$', 'hfjs.views.index'),
-    url(r'(?P<template>.+\.html)$', 'django.views.generic.simple.direct_to_template'), 
+urlpatterns = patterns('hfjs.views',
+    url(r'^$', 'index'),
+    url(r'donuts_result/$', 'donuts_result'),
 )
 
+urlpatterns += patterns('',
+    url(r'(?P<template>.+\.html)$', 'django.views.generic.simple.direct_to_template'), 
+)
--- views.py
+++ views.py
@@ -1,5 +1,14 @@
 # Create your views here.
 from django.shortcuts import render_to_response
+import random
 
 def index(request):
     return render_to_response('hfjs_home.html')
+
+def donuts_result(request):
+    return render_to_response('donuts/donuts_result.html', {
+        'order_num': random.randint(1000, 10000),
+        'customer_name': request.POST.get('name', ''),
+        'ready_in_mins': request.POST.get('pickupminutes', ''),
+        'total_price': request.POST.get('total', ''),
+})
