a1c1e47f0dc62ed50b1581aba0f21282c9a11d63
bingo2011 <bin.go2011@foxmail.com>
2013-02-14 09:53:38 +0800

    bannerocity for form and validation

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('hfjs.views',
     url(r'^$', 'index'),
     url(r'donuts_result/$', 'donuts_result'),
+    url(r'bannerocity_result/$', 'bannerocity_result'),
 )
 
 urlpatterns += patterns('',
--- views.py
+++ views.py
@@ -12,3 +12,13 @@ def donuts_result(request):
         'ready_in_mins': request.POST.get('pickupminutes', ''),
         'total_price': request.POST.get('total', ''),
 })
+
+def bannerocity_result(request):
+    return render_to_response('bannerocity/bannerocity_result.html', {
+        'order_num': random.randint(1000, 10000),
+        'message': request.POST.get('message', ''),
+        'zipcode': request.POST.get('zipcode', ''),
+        'date': request.POST.get('date', ''),
+        'phone': request.POST.get('phone', ''),
+        'email': request.POST.get('email', ''),
+})
